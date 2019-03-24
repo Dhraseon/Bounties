f9808bcc6fad4d265b0d56aed8b36ccc2925b67e
root <root@localhost.localdomain>
2019-03-24 19:49:42 +0000

    Added if to split and fixed file moving. Manully moved finished files

--- Dell/Scripts/nmap_me.sh
+++ Dell/Scripts/nmap_me.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # split the domain files into groups of 50
+# iff the split dir doesn't exist already
 FILES="../Domains/Sublist3r/*"
-mkdir "../Domains/Split"
-for f in $FILES
-do
-	name=${f##*/}
-	echo "[*] Splitting ${name}"
-	split -l 50 $f ../Domains/Split/
-done
+if [! -d "../Domains/Split"]; then
+	mkdir "../Domains/Split"
+	for f in $FILES
+	do
+		name=${f##*/}
+		echo "[*] Splitting ${name}"
+		split -l 50 $f ../Domains/Split/
+	done
+fi
 # Nmap each file created by the split
 mkdir "../Domains/Done/"
 FILES="../Domains/Split/*"
@@ -17,5 +20,5 @@ do
 	echo "[*] Nmapping: ${name}"
 	nmap -iL $f -oG "./../Domains/Nmap/${name}_nmap.txt"
 	echo "[*] Nmap done, moving file ${name} to ../Domains/Split/" 
-	mv $f "../Domains/Split/${name}"
+	mv $f "../Domains/Done/${name}"
 done
