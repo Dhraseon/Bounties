d9d8d5375014a074cc4f958f6cbe0e17cfc67c37
root <root@localhost.localdomain>
2019-03-25 18:17:19 +0000

    Wrote beta for setup.sh

---
+++ Scripts/setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# get dir root from user, this should be the name of the company
+echo "[*] Enter name: "
+name="test"
+#set up sub-dirs and array
+domains="${name}/Domains"
+done="${domains}/Done"
+split="${domains}/Split"
+nmap="${domains}/Nmap"
+sublister="${domains}/Sublist3r"
+extension="${name}/Extensions"
+retire="${extension}/Retire"
+note="${name}/Notes"
+scriptDir="${name}/Scripts"
+declare -a dirs=(
+$domains, $done, $split,
+$nmap, $sublister, $extension,
+$retire, $note, $scriptDir
+	        )
+
+# iteratre through array
+echo "[*] Making directory tree"
+for dir in "${dirs[@]}"
+do
+	mkdir $dir
+done
+
+# copy scripts over
+echo "[*] Making scripts"
+nmap="nmap.sh"
+declare -a scripts=(
+$nmap
+		   )
+for script in "${scripts[@]}"
+do
+	cp $script $scriptDir
+done
+echo "[*] Done!"
