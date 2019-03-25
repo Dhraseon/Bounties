8a18efb31b98e9effedc7dae03c51ade844a5bec
root <root@localhost.localdomain>
2019-03-25 18:06:58 +0000

    Nmapped stuff and copied nmap script

---
+++ Scripts/nmap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# split the domain files into groups of 50
+# iff the split dir doesn't exist already
+FILES="../Domains/Sublist3r/*"
+if [! -d "../Domains/Split"]; then
+	mkdir "../Domains/Split"
+	for f in $FILES
+	do
+		name=${f##*/}
+		echo "[*] Splitting ${name}"
+		split -l 50 $f ../Domains/Split/
+	done
+fi
+# Nmap each file created by the split
+mkdir "../Domains/Done/"
+FILES="../Domains/Split/*"
+for f in $FILES
+do
+	name=${f##*/}
+	echo "[*] Nmapping: ${name}"
+	nmap -iL $f -oG "./../Domains/Nmap/${name}_nmap.txt"
+	echo "[*] Nmap done, moving file ${name} to ../Domains/Done/" 
+	mv $f "../Domains/Done/${name}"
+done
