4b4d1202023d0ebc36ba0738a05c74ac28ed70c8
root <root@localhost.localdomain>
2019-03-25 17:24:50 +0000

    Wrote to-do for scripting and chrome scipt, need to test

---
+++ Scripts/chrome.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# download keys and repo
+cd "~/Downloads/"
+line="deb http://dl.google.com/linux/chrome/deb/ stable main"
+echo $line >> /etc/apt/sources.list
+key="https://dl-ssl.google.com/linux/linux_signing_key.pub"
+wget $key
+key="linux_signing_key.pub"
+# install and update
+sudo apt-key add $key
+sudo apt-get update
+sudo apt-get install google-chrome-stable
